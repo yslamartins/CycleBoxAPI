7c71bc1093925b45e0ccf6007b200413e9a708f3
Jhonatan Gonçalves <jhonatangoncalves2002@gmail.com>
2025-03-22 18:15:20 -0300

    bugfix: use SECRET jwt and duplicate in atutentication

--- handlers/authHandlers.js
+++ handlers/authHandlers.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken"); 
 const { PrismaClient } = require("@prisma/client");
 require("dotenv").config();
 
@@ -8,41 +8,36 @@ const prisma = new PrismaClient();
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
-    const { name, email, password } = req.body;
-  
-    if (!name || !email || !password) {
-      return res.status(400).json({ message: "Todos os campos são obrigatórios!" });
-    }
-  
-    const hashedPassword = await bcrypt.hash(password, 10);
-  
-    try {
-      const user = await prisma.user.create({
-        data: { name, email, password: hashedPassword },
-      });
-      res.status(201).json({ message: "Usuário criado com sucesso!" });
-    } catch (error) {
-      res.status(400).json({ message: "Erro ao criar usuário", error });
-    }
-  });
+  const { name, email, password } = req.body;
 
-  const jwt = require("jsonwebtoken");
-  require("dotenv").config();
-  
-  router.post("/login", async (req, res) => {
-    const { email, password } = req.body;
-  
-    const user = await prisma.user.findUnique({ where: { email } });
-  
-    if (!user || !(await bcrypt.compare(password, user.password))) {
-      return res.status(401).json({ message: "Credenciais inválidas" });
-    }
-  
-    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: "1h" });
-  
-    res.json({ token });
-  });
-  
-  
+  if (!name || !email || !password) {
+    return res.status(400).json({ message: "Todos os campos são obrigatórios!" });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  try {
+    const user = await prisma.user.create({
+      data: { name, email, password: hashedPassword },
+    });
+    res.status(201).json({ message: "Usuário criado com sucesso!" });
+  } catch (error) {
+    res.status(400).json({ message: "Erro ao criar usuário", error });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await prisma.user.findUnique({ where: { email } });
+
+  if (!user || !(await bcrypt.compare(password, user.password))) {
+    return res.status(401).json({ message: "Credenciais inválidas" });
+  }
+
+  const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+
+  res.json({ token });
+});
 
 module.exports = router;
