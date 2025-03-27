8cfae8ae2f8d5ef9f722213295b9f1ca2d5fd9be
yslamartins <claudiaysla101@gmail.com>
2025-03-27 17:47:53 -0300

    docs: implementation of swagger documentation

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors"); 
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
 const userRoutes = require("./routes/userRoutes");
 const productRoutes = require("./routes/productRoutes");
 
@@ -7,9 +9,37 @@ const app = express();
 
 app.use(express.json());
 app.use(cors()); 
+
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando http://localhost:${PORT}`);
+});
+
+
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "CycleBox API",
+      version: "1.0.0",
+      description: "Documentação da API do brechó CycleBox",
+    },
+    servers: [
+      {
+        url: `http://localhost:${PORT}`, // 
+        description: "Servidor Local",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 app.use("/users", userRoutes);
 app.use(productRoutes);
 
-app.listen(4000, () => {
-  console.log("Servidor rodando na porta 4000");
+app.get('/', (req, res) => {
+  res.send('Servidor está funcionando!');
 });
